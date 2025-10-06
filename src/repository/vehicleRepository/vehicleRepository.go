fa07f6715dd1c4fc6711c8efbd1e72f31e41deca
Caio Pereira <caiomp87@gmail.com>
2025-10-06 18:20:48 -0300

    implement reposutory layer

--- src/repository/vehicleRepository/vehicleRepository.go
+++ src/repository/vehicleRepository/vehicleRepository.go
@@ -2,8 +2,12 @@ package vehicleRepository
 
 import (
 	"context"
+	"time"
 
 	"github.com/caiomp87/vehicle-resale-api/src/core/domain/entity"
+	"github.com/caiomp87/vehicle-resale-api/src/repository/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -15,27 +19,117 @@ type VehicleRepository interface {
 }
 
 type vehicleRepository struct {
-	db *mongo.Database
+	collection *mongo.Collection
 }
 
-func NewVehicleRepository(db *mongo.Database) VehicleRepository {
+func NewVehicleRepository(collection *mongo.Collection) VehicleRepository {
 	return &vehicleRepository{
-		db: db,
+		collection: collection,
 	}
 }
 
 func (ref *vehicleRepository) Create(ctx context.Context, vehicle entity.Vehicle) (*entity.Vehicle, error) {
-	panic("unimplemented")
+	record := model.VehicleFromDomain(vehicle)
+	record.Sold = false
+
+	now := time.Now()
+	record.CreatedAt = now
+	record.UpdatedAt = now
+
+	created, err := ref.collection.InsertOne(ctx, record)
+	if err != nil {
+		return nil, err
+	}
+
+	id := created.InsertedID.(primitive.ObjectID)
+
+	result := ref.collection.FindOne(ctx, bson.M{"_id": id})
+	if err = result.Err(); err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	var recordToReturn model.Vehicle
+	if err = result.Decode(&recordToReturn); err != nil {
+		return nil, err
+	}
+
+	return recordToReturn.ToDomain(), nil
 }
 
 func (ref *vehicleRepository) GetByID(ctx context.Context, id string) (*entity.Vehicle, error) {
-	panic("unimplemented")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	result := ref.collection.FindOne(ctx, bson.M{"_id": objectID})
+	if err = result.Err(); err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	var record model.Vehicle
+	if err = result.Decode(&record); err != nil {
+		return nil, err
+	}
+
+	return record.ToDomain(), nil
 }
 
 func (ref *vehicleRepository) Search(ctx context.Context) ([]entity.Vehicle, error) {
-	panic("unimplemented")
+	cursor, err := ref.collection.Find(ctx, nil)
+	if err != nil {
+		if err == mongo.ErrNilCursor {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	records := make([]entity.Vehicle, 0)
+
+	for cursor.Next(ctx) {
+		var record model.Vehicle
+		if err = cursor.Decode(&record); err != nil {
+			return nil, err
+		}
+
+		records = append(records, *record.ToDomain())
+	}
+
+	return records, nil
 }
 
 func (ref *vehicleRepository) Update(ctx context.Context, id string, vehicle entity.Vehicle) (*entity.Vehicle, error) {
-	panic("unimplemented")
+	record := model.VehicleFromDomain(vehicle)
+	record.UpdatedAt = time.Now()
+
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = ref.collection.UpdateOne(ctx, bson.M{"_id": objectID}, record)
+	if err != nil {
+		return nil, err
+	}
+
+	result := ref.collection.FindOne(ctx, bson.M{"_id": objectID})
+	if err = result.Err(); err != nil {
+		if err == mongo.ErrNoDocuments {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	var recordToReturn model.Vehicle
+	if err = result.Decode(&record); err != nil {
+		return nil, err
+	}
+
+	return recordToReturn.ToDomain(), nil
 }
